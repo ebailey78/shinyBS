d9cd83f2bf6aa775d334276990b1bc81b2eaa589
Eric Bailey <ebailey@idem.in.gov>
2014-02-03 11:07:53 -0500

    Finished documentation!!! (For now...)

--- R/bsNavText.R
+++ R/bsNavText.R
@@ -1,4 +1,4 @@
 # same as textOutput but wrapped for better formatting in navbar 
-bsNavTextOutput <- function(inputId, value=NULL) {
-          tags$li(tags$p(id = inputId, class="navbar-text shiny-text-output", value))
+bsNavTextOutput <- function(outputId) {
+          tags$li(tags$p(id = inputId, class="navbar-text shiny-text-output"))
 }
