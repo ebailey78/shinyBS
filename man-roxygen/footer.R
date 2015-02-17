ed7bb69cec0038abd7633d0a488b77d03764c1e3
Eric <eb11307@gmail.com>
2015-02-17 14:48:03 -0500

    Starting Over

--- man-roxygen/footer.R
+++
@@ -1,5 +0,0 @@
-#'@note Run \code{bsDemo()} for a live examples of shinyBS functionality. Currently Disabled
-#'@author Eric Bailey
-#'@references \href{http://getbootstrap.com}{Twitter Bootstrap 3}
-#'@examples \dontrun{Run bsDemo() for examples}
-#'@export
