baab1a318d8c0618f16f3c462f568b7c4dd9a548
ebailey78 <ebailey@idem.in.gov>
2015-02-17 22:25:47 -0500

    adding code highlighting to demo

--- inst/demo/ui.R
+++ inst/demo/ui.R
@@ -1,19 +1,47 @@
 library(shiny)
 library(shinyBS)
 
-demoPanel <- function(title, controls, example) {
+demoPanel <- function(title, intro, controls, example) {
   tabPanel(title,
-    fluidRow(
-      column(3, wellPanel(controls)),
-      column(9, example)
+    fluidRow(style = 'padding-top: 10px;',
+      column(8, 
+        fluidRow(
+          column(12, intro)
+        ),
+        fluidRow(
+          column(4, wellPanel(controls)),
+          column(8, example)
+        )
+      ),
+      column(4,
+        fluidRow(
+          column(12, "ui.R", tags$pre(style = "background: none;", tags$code(id = paste(gsub(" ", "_", title, fixed = TRUE), "ui", sep = "_"), clas = "r", "library(shiny)
+library(shinyBS)")))
+        ),
+        fluidRow(
+          column(12, "server.R", tags$pre(style = "background: none;", tags$code(id = paste(gsub(" ", "_", title, fixed = TRUE), "ui", sep = "_"), clas = "r", "library(shiny)
+library(shinyBS)
+
+shinyServer(function(input, output, session) {
+                                                                                 
+
+}")))
+        )
+      )   
     )
   )
 }
 
 fluidPage(
+  tags$head(
+    tags$link(rel = "stylesheet", href = "styles/xcode.css"),
+    tags$script(src = "highlight.pack.js"),
+    tags$script("hljs.initHighlightingOnLoad();")
+  ),
   titlePanel("shinyBS Demo"),
-  tabsetPanel(id = "bsDemo",
+  tabsetPanel(id = "bsDemo", type = "pills",
     demoPanel("Alerts", 
+              intro = "Alerts offer a way to convey information to the user about what is happening behind the scenes or how their selections are affecting the shiny app.",
               controls = tagList(textInput("bsAlertTitle", "Alert Title", value = ""),
                                  textInput("bsAlertMessage", "Alert Message", value = "This is an alert."),
                                  selectInput("bsAlertType", "Alert Type", choices = c("warning", "danger", "info", "success"), selected = "info"),
