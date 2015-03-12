c65fd2f1c270386f20098893b9f5a8ab762c2422
Eric <eb11307@gmail.com>
2015-03-12 14:06:42 -0400

    added check for existing alert before creating new alert

--- inst/www/shinyBS.js
+++ inst/www/shinyBS.js
@@ -137,40 +137,52 @@ Shiny.inputBindings.register(shinyBS.inputBindings.collapse);
 
 Shiny.addCustomMessageHandler("bsAlertCreate", function(data) {
 
-  var $alert = $("<div class = 'alert'></div>");
+  var create = true;
   
-  if(data.hasOwnProperty('style')) {
-    $alert.addClass("alert-" + data.style);
-  } else {
-    $alert.addClass("alert-info");
-  }
-  
-  if(data.hasOwnProperty("dismiss")) {
-    $alert.addClass("alert-dismissable");
-  }
-
   if(data.hasOwnProperty("alertId")) {
-    $alert.attr("id", data.alertId);
-  }
-  
-  if(data.hasOwnProperty('dismiss')) {
-    if(data.dismiss == true) {
-      $alert.append("<button type='button' class='close' data-dismiss='alert'>&times;</button>")
+    if($("#" + data.alertId).length > 0) {
+      create = false;
     }
   }
 
-  if(data.hasOwnProperty('title')) {
-    $alert.append("<h4>" + data.title + "</h4>");
-  }
-  
-  if(data.hasOwnProperty("content")) {
-    $alert.append(data.content);
-  }
+  if(create) {
 
-  if(data.append == true) {
-    $alert.appendTo("#" + data.id);
-  } else {
-    $("#" + data.id).html($alert);
+    var $alert = $("<div class = 'alert'></div>");
+    
+    if(data.hasOwnProperty('style')) {
+      $alert.addClass("alert-" + data.style);
+    } else {
+      $alert.addClass("alert-info");
+    }
+    
+    if(data.hasOwnProperty("dismiss")) {
+      $alert.addClass("alert-dismissable");
+    }
+  
+    if(data.hasOwnProperty("alertId")) {
+      $alert.attr("id", data.alertId);
+    }
+    
+    if(data.hasOwnProperty('dismiss')) {
+      if(data.dismiss == true) {
+        $alert.append("<button type='button' class='close' data-dismiss='alert'>&times;</button>")
+      }
+    }
+  
+    if(data.hasOwnProperty('title')) {
+      $alert.append("<h4>" + data.title + "</h4>");
+    }
+    
+    if(data.hasOwnProperty("content")) {
+      $alert.append(data.content);
+    }
+  
+    if(data.append == true) {
+      $alert.appendTo("#" + data.id);
+    } else {
+      $("#" + data.id).html($alert);
+    }
+    
   }
 
 });
