f2e2fc85cb2b85a7cc365830417ebb5c69b6934a
Eric <eb11307@gmail.com>
2015-03-31 14:09:22 -0400

    Working to integrate ThomasSiegmund's shinyTypeahead package to return typeahead functionality.

--- R/misc.R
+++ R/misc.R
@@ -6,6 +6,7 @@
 }
 
 shinyBSDep <- htmltools::htmlDependency("shinyBS", packageVersion("shinyBS"), src = c("href" = "sbs"), script = "shinyBS.js", stylesheet = "shinyBS.css")
+typeaheadDep <- htmltools::htmlDependency("shinyBS", packageVersion("shinyBS"), src = c("href" = "sbs"), script = c("bootstrap3-typeahead.js", "typeahead_inputbinding.js"));
 
 # Copy of dropNulls function for shiny to avoid using shiny:::dropNulls
 dropNulls <- function(x) {
