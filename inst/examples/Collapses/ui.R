76bf5065d0f4ce7264f9ea9d1a907835eef974fa
Gregory R Warnes <greg@warnes.net>
2021-01-06 15:48:36 -0500

    Add `id` parameter to `bsCollapsePanel` to set the HTML id of the collapse element.

--- inst/examples/Collapses/ui.R
+++ inst/examples/Collapses/ui.R
@@ -1,21 +1,41 @@
 library(shiny)
 library(shinyBS)
- fluidPage(
+fluidPage(
    sidebarLayout(
-     sidebarPanel(HTML("This button will open Panel 1 using <code>updateCollapse</code>."),
-                  actionButton("p1Button", "Push Me!"),
-                  selectInput("styleSelect", "Select style for Panel 1",
-                   c("default", "primary", "danger", "warning", "info", "success"))
-     ),
-     mainPanel(
-       bsCollapse(id = "collapseExample", open = "Panel 2",
-                  bsCollapsePanel("Panel 1", "This is a panel with just text ",
-                   "and has the default style. You can change the style in ",
-                   "the sidebar.", style = "info"),
-                  bsCollapsePanel("Panel 2", "This panel has a generic plot. ",
-                   "and a 'success' style.", plotOutput("genericPlot"), style = "success")
-       )
-     )
+      sidebarPanel(
+         div(
+            HTML("This button will open Panel 1 using <code>updateCollapse</code>."),
+            actionButton("p1Button", "Push Me!")
+         ),
+         div(
+            HTML("This button will toggle Panel 1 <strong>without</strong> using <code>updateCollapse</code>."),
+            actionButton(
+               inputId="p2Button",
+               "Push Me!",
+               class="btn btn-primary",
+               type="button",
+               "data-toggle"="collapse",
+               "data-target"="#panel1",
+               "aria-expanded"="false",
+               "aria-controls"="collapseExample"
+            )
+         ),
+         selectInput("styleSelect", "Select style for Panel 1",
+                     c("default", "primary", "danger", "warning", "info", "success"))
+      ),
+      mainPanel(
+         bsCollapse(
+            id = "collapseExample", 
+            open = "Panel 2",
+            bsCollapsePanel("Panel 1", 
+                            id="panel1",
+                            "This is a panel with just text ",
+                            "and has the default style. You can change the style in ",
+                            "the sidebar.", style = "info"),
+            bsCollapsePanel("Panel 2", "This panel has a generic plot. ",
+                            "and a 'success' style.", plotOutput("genericPlot"), style = "success")
+         )
+      )
    )
 )
 
